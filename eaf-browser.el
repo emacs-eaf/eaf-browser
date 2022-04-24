c6c92186b317b4ceae8fec81ed35195fb5e7143d
Andy Stewart <lazycat.manatee@gmail.com>
2022-04-24 15:17:53 +0800

    Move eaf-open-devtool-page to EAF core.

--- eaf-browser.el
+++ eaf-browser.el
@@ -653,13 +653,6 @@ Otherwise send key 'esc' to browser."
           "Switch to org-mode with `\\[eaf-edit-buffer-switch-to-org-mode]'. "
           ))))
 
-(defun eaf-open-devtool-page ()
-  "Use EAF Browser to open the devtools page."
-  (delete-other-windows)
-  (split-window (selected-window) (/ (* (nth 3 (eaf-get-window-allocation (selected-window))) 2) 3) nil t)
-  (other-window 1)
-  (eaf-open "about:blank" "browser" "devtools"))
-
 (defun eaf--toggle-caret-browsing (caret-status)
   "Toggle caret browsing given CARET-STATUS."
   (if caret-status
